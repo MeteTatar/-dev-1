da81d4849d4c92eb4ee9d4585040f7016b899ecc
Mete Tatar <mete-tatar@hotmail.com>
2023-05-26 13:42:39 +0300

    Update

--- javadoc.js
+++ javadoc.js
@@ -3,3 +3,12 @@ let hiname = document.querySelector("#names")
 hiname = prompt("Adınızı Giriniz = ")
 
 names.innerHTML = `${ hiname}! `
+
+function tarihSaat() 
+{
+
+var date = new Date().toLocaleString('tr-TR');
+document.getElementById("zaman").innerHTML = date;
+
+} // her 1 saniyede tarihSaat fonksiyonunu yeniden çalıştır
+setInterval(tarihSaat, 1000); 
